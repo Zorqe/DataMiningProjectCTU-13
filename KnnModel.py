703ec708c293a3457ce47df76f127922cc7cb27b
Zaid <zkhal015@uottawa.ca>
2019-03-18 19:58:47 -0400

    Updated both the adaBoostDT model and KNN model
    Adaboost now uses proper features, displays summary of results and implemented saving/loading model through .pkl
    KNN model now uses proper features and the new discretized data

--- KnnModel.py
+++ KnnModel.py
@@ -5,14 +5,14 @@ from sklearn.metrics import accuracy_score
 
 #Train dataset to use for classification
 #dataFrameTrain = pd.read_csv('trainDataSetFeatureGenerated.csv')
-#dataFrameTrain = pd.read_csv('capture20110815-2FeatureGenerated.csv')
 #dataFrameToPredict = pd.read_csv('trainDataSetFeatureGenerated.csv')
 
 
 #--------------------TEMPORARY----------------------
-dataFrame = pd.read_csv('testing100000RowsFeatureGenerated.csv')
-dataFrameTrain = dataFrame[0:85000]
-dataFrameTest = dataFrame[85000:]
+dataFrame = pd.read_csv('capture20110818ResampledFeatureGenerated.csv')
+
+dataFrameTrain = dataFrame[0:390000]
+dataFrameTest = dataFrame[390000:]
 
 
 #Mapping taken from tutorial https://www.kaggle.com/parasjindal96/how-to-normalize-dataframe-pandas
@@ -29,15 +29,6 @@ def mapping(data,feature):
 
 #Function to normalize all data for similarity calculations is KNN
 def normalizeData(dfTrain):
-    stringColsToMap = ['Sport','Dport','TotBytesDisc','SrcBytesDisc','SportDisc','DportDisc','Src_TotBytesDisc_mode','Dst_TotBytesDisc_mode']
-    
-    dfTrain['Sport']=pd.to_numeric(dfTrain['Sport'], errors='coerce')
-    dfTrain['Dport']=pd.to_numeric(dfTrain['Dport'], errors='coerce')
-
-
-    for col in stringColsToMap:
-        dfTrain = mapping(dfTrain,col)
-
 
     #Normalizing all values between 0 to 15
     dfNormalized = ((dfTrain-dfTrain.min())/(dfTrain.max()-dfTrain.min()))*15
@@ -63,8 +54,8 @@ def knnPredict(k, dataFrameTest,dataFrameTrain):
 
 
 #Drop all columns which we won't use for training data, instead use their discretized values
-dataFrameTrain = dataFrameTrain.drop(['StartTime','SrcAddr','DstAddr','State','Label','Proto','SrcAddr_App','SrcDst_Sport_unique'], axis=1)
-dataFrameTest = dataFrameTest.drop(['StartTime','SrcAddr','DstAddr','State','Label','Proto','SrcAddr_App','SrcDst_Sport_unique'], axis=1)
+dataFrameTrain = dataFrameTrain.drop(['StartTime','SrcAddr','DstAddr','State','SrcAddr_App','SrcDst_Sport_unique'], axis=1)
+dataFrameTest = dataFrameTest.drop(['StartTime','SrcAddr','DstAddr','State','SrcAddr_App','SrcDst_Sport_unique'], axis=1)
 
 dataFrameTrain = normalizeData(dataFrameTrain)
 dataFrameTest = normalizeData(dataFrameTest)
@@ -74,10 +65,19 @@ k = 1
 print("\nPredicting values with k value: " + str(k))
 predictions = knnPredict(k, dataFrameTest, dataFrameTrain)
 
-print("The predicted discretized labels are: \n"+ str(predictions))
+print("Set of predictions are: " + str(set(predictions)) )
+print("The count of malicious predictions are: "  + str( np.count_nonzero(predictions == 15.0) )  )
+print("The count of background predictions are: "  + str( np.count_nonzero(predictions == 0.0) )  )
 
 #Calculating the accuracy
 accuracy =  accuracy_score(dataFrameTest['LabelDisc'], predictions) *100
 print ("The accuracy is: " + str(accuracy)+"%")
 
-print(str(set(predictions)))
+correctlyClassified = 0
+index = 0
+for classification in list(dataFrameTest.LabelDisc):
+    if predictions[index] == classification and classification == 15.0:
+        correctlyClassified += 1
+    index += 1
+
+print("The number of correctly classified malicious predictions are: " + str(correctlyClassified))
--- adaBoostDT.py
+++ adaBoostDT.py
@@ -4,79 +4,92 @@ from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier,
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
+import os
+import pickle
 
 
 #Inspired from: https://www.kaggle.com/treina/titanic-with-adaboost
-
-#Function to normalize all data for similarity calculations is KNN
-def normalizeData(dfTrain):
-    stringColsToMap = ['TotBytesDisc','SrcBytesDisc','SportDisc','DportDisc','Src_TotBytesDisc_mode','Dst_TotBytesDisc_mode']
-
-
-    dfNormalized = dfTrain
-    dfNormalized['Sport']=pd.to_numeric(dfNormalized['Sport'], errors='coerce')
-    dfNormalized['Dport']=pd.to_numeric(dfNormalized['Dport'], errors='coerce')
-
-    for col in stringColsToMap:
-        LE = LabelEncoder()
-        dfNormalized[col] = LE.fit_transform(dfNormalized[col])
-    
-    dfNormalized = dfNormalized.fillna(0)
-
-    return dfNormalized
-
-
-
-
-
 #---Main---
-
-
-dataFrame = pd.read_csv('testing100000RowsFeatureGenerated.csv',encoding="utf-8")
-
-train_dataFrame = dataFrame[0:85000]
-test_dataFrame = dataFrame[85000:]
-
-#Drop columns to not train on
-train_dataFrame = train_dataFrame.drop(['StartTime','SrcAddr','DstAddr','State','Label','Proto'], axis=1)
-test_dataFrame = test_dataFrame.drop(['StartTime','SrcAddr','DstAddr','State','Label','Proto'], axis=1)
-
-train_dataFrame = normalizeData(train_dataFrame)
-test_dataFrame = normalizeData(test_dataFrame)
-
-train_dataframe_Features = train_dataFrame[['LabelDisc']].copy()
-train_dataFrame = train_dataFrame.drop(['LabelDisc'], axis=1)
-
-test_dataFrame_Features = test_dataFrame[['LabelDisc']].copy()
-test_dataFrame = test_dataFrame.drop(['LabelDisc'], axis=1)
-
-#-------Training--------
-#Getting the values for the data frame as well as the results (0 normal, 1 malicious)
-train_data = train_dataFrame.values
-train_data_results = train_dataframe_Features.values
-
-test_data = test_dataFrame.values
-test_data_results = test_dataFrame_Features.values
-
-#Storing the features to use in model as well as the classifications
-train_features = train_data[0::]
-train_result = train_data_results[0::,0]
-
-test_features = test_data[0::]
-test_results = test_data_results[0::,0]
-
-print("Model training...")
 
 #An adaboost DT classifier using RandomForests
 model = AdaBoostClassifier(RandomForestClassifier(n_estimators = 1000),
-                         algorithm="SAMME",
-                         n_estimators=500)
+                        algorithm="SAMME",
+                        n_estimators=500)
 
-#Fit the training data to the Adaboost model
-model = model.fit(train_features, train_result)
+dataFrame = pd.read_csv('capture20110818ResampledFeatureGenerated.csv')
+test_dataFrame = dataFrame[375000:]
+
+#Dropping values that weren't trained on
+test_dataFrame = test_dataFrame.drop(['StartTime','SrcAddr','DstAddr','State'], axis=1)
+
+#Splitting up the test dataframe into one with only features, other with classifications
+test_dataFrame_Classification = test_dataFrame[['LabelDisc']].copy()
+test_dataFrame = test_dataFrame.drop(['LabelDisc'], axis=1)
+
+test_data = test_dataFrame.values
+test_data_results = test_dataFrame_Classification.values
+
+#Getting vals to use for testing
+test_features = test_data[0::]
+test_results = test_data_results[0::,0]
+
+#Model doesn't exist so make model and then save it
+if not(os.path.exists("adaBoostModel.pkl")):
+
+    train_dataFrame = dataFrame[0:375000]
+
+    #Drop columns to not train on
+    train_dataFrame = train_dataFrame.drop(['StartTime','SrcAddr','DstAddr','State'], axis=1)
+
+    train_dataframe_Classification = train_dataFrame[['LabelDisc']].copy()
+    train_dataFrame = train_dataFrame.drop(['LabelDisc'], axis=1)
+
+    #-------Training--------
+    #Getting the values for the data frame as well as the results (0 normal, 1 malicious)
+    train_data = train_dataFrame.values
+    train_data_results = train_dataframe_Classification.values
+
+    #Storing the features to use in model as well as the classifications
+    train_features = train_data[0::]
+    train_result = train_data_results[0::,0]
+
+
+    print("Model training...")
+
+    #Fit the training data to the Adaboost model
+    model = model.fit(train_features, train_result)
+
+    #Save model
+    #Source(save model): https://stackabuse.com/scikit-learn-save-and-restore-models/
+    model_filename = "adaBoostModel.pkl"
+    with open(model_filename, 'wb') as file:  
+        pickle.dump(model, file)
+
+#Model exists so load it
+else:
+    print("Loading model...")
+    # Load from file
+    #Source(load model): https://stackabuse.com/scikit-learn-save-and-restore-models/
+    model_filename = "adaBoostModel.pkl"  
+    with open(model_filename, 'rb') as file:  
+        model = pickle.load(file)
 
 print("Predicting...")
 
 prediction = model.predict(test_features)
-print(str(set(prediction)))
-print ('Accuracy = {:0.2f}%'.format(100.0 * accuracy_score(test_results, prediction)))
+
+print("Set of predictions are: " + str(set(prediction)) )
+print("The count of malicious predictions are: "  + str( np.count_nonzero(prediction == 1) )  )
+print("The count of background predictions are: "  + str( np.count_nonzero(prediction == 0) )  )
+
+
+print ('\nAccuracy = {:0.2f}%'.format(100.0 * accuracy_score(test_results, prediction)))
+
+correctlyClassified = 0
+index = 0
+for classification in list(test_dataFrame_Classification.LabelDisc):
+    if prediction[index] == classification and classification == 1:
+        correctlyClassified += 1
+    index += 1
+
+print("The number of correctly classified malicious predictions are: " + str(correctlyClassified))
